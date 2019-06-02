b3cb882e2ab08df8f6b2bb493cc956527c092fef
Fahim Tajwar <tajwarfahim@gmail.com>
2019-06-02 13:05:31 -0700

    label reader complete

--- project_code/read_labels.py
+++ project_code/read_labels.py
@@ -1,20 +1,44 @@
 import numpy as np
 import pandas as pd
+from project_code.util import *
 
+# constants
+ALLOWED_LABELS = ['cell dies', 'grows sparse', 'grows dense']
+
+# helper functions
 def read_excel_file(file_name):
     data_frame = pd.read_excel(file_name)
     return data_frame
 
-def read_labels(data_frame):
-    num_rows, num_cols = data_frame.shape
-    map_of_labels = {}
+def get_well_id_from_well_name(well_name):
+    well_name = str(well_name)
+    assert(len(well_name) > 0)
+    if well_name[0] == 'w':
+        return int(well_name[4:])
+    else:
+        return int(well_name)
 
-    for i in range(0, num_rows):
-        well_name = data_frame.iloc[i, 0]
-        well_id = int(well_name[4:])
-        map_of_labels[well_id] = data_frame.iloc[i, 1]
+def get_well_label_from_given_label(given_label):
+    if given_label == 'sparse' or given_label == 'grows sparse':
+        return 'grows sparse'
+    elif given_label == 'grows dense' or given_label == 'dense':
+        return 'grows dense'
+    elif given_label == 'cell dies' or given_label == 'dies':
+        return 'cell dies'
+    else:
+        return given_label
 
-    return map_of_labels
+def read_labels(file_names, allowed_labels):
+    label_map = {}
+    for file_name in file_names:
+        data_frame = read_excel_file(file_name)
+        num_rows, num_cols = data_frame.shape
+        for i in range(num_rows):
+            well_id = get_well_id_from_well_name(data_frame.iloc[i, 0])
+            well_label = get_well_label_from_given_label(data_frame.iloc[i, 1])
+            if well_label in ALLOWED_LABELS:
+                label_map[well_id] = well_label
+    return label_map
 
 def find_type_of_labels(label_map):
     set_of_labels = set()
@@ -53,19 +77,33 @@ def create_label_vector(label_map, label_to_label_id):
     label_vector = []
 
     for well_id in range(0, max_well_id):
-        label = label_map[well_id]
-        label_id = label_to_label_id[label]
-        label_vector.append(label_id)
+        if well_id in label_map:
+            label = label_map[well_id]
+            label_id = label_to_label_id[label]
+            label_vector.append(label_id)
 
     return label_vector
 
+def get_class_distribution(label_id_to_label_map, label_vector):
+    class_distribution = {}
+    for i in range(len(label_vector)):
+        label = label_vector[i]
+        if label_id_to_label_map[label] in class_distribution:
+            class_distribution[label_id_to_label_map[label]] += 1
+        else:
+            class_distribution[label_id_to_label_map[label]] = 1
+
+    return class_distribution
+
+# the abstract class that takes care of these things for us
 class Label_Reader:
-    def __init__(self, file_name):
-        data_frame = read_excel_file(file_name)
-        self.label_map = read_labels(data_frame)
+    def __init__(self, file_names):
+        self.label_map = read_labels(file_names)
         self.type_of_labels, self.set_of_labels = find_type_of_labels(self.label_map)
         self.label_to_label_id, self.label_id_to_label = enumerate_labels(self.set_of_labels)
         self.label_vector = create_label_vector(self.label_map, self.label_to_label_id)
+        self.num_data_points = len(self.label_map)
+        self.class_distribution = get_class_distribution(self.label_id_to_label, self.label_vector)
 
     def get_label_vector(self):
         return self.label_vector
@@ -81,3 +119,13 @@ class Label_Reader:
 
     def get_all_labels(self):
         return self.set_of_labels
+
+    def get_number_of_data_points(self):
+        return self.num_data_points
+
+    def get_class_distribution(self):
+        return self.class_distribution
+
+    def show_class_disribution_histogram(self):
+        label_for_each_class = list(self.label_map.values())
+        plot_bar_graph_from_map(self.class_distribution, "Classes", "Frequency", label_for_each_class)
---
+++ project_code/util.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mp
+
+def show_plot(array, x_title = None, y_title = None):
+    x_axis = range(len(array))
+    y_axis = array
+
+    plt.plot(x_axis, y_axis)
+    if x_title != None:
+        plt.xlabel(x_title)
+    if y_title != None:
+        plt.ylabel(y_title)
+
+    plt.show()
+
+def plot_bar_graph_from_map(map, x_label, y_label, label_for_each_class):
+    plt.bar(range(len(map)), list(map.values()), align='center')
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.xticks(range(len(map)), label_for_each_class)
+    plt.show()
